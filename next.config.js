c9cae96822bfc298de76d271388b44a18cbf0e51
Johanss <johanss.zip@gmail.com>
2024-04-09 14:44:58 +0800

    Cors updates

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.ALLOW_ORIGIN_DOMAIN || "",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
