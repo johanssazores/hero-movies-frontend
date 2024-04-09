f0eae7171e1521a86b8ff32a356b6efd4763e08f
Johanss <johanss.zip@gmail.com>
2024-04-09 14:34:44 +0800

    Simple Remove

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,7 +6,9 @@ const Menu = () => {
   const { data: session }: any = useSession();
   return (
     <div className="flex items-center justify-between border-b border shadow-lg py-6 px-8">
-      <h1 className="font-bold text-2xl">Hero Movies</h1>
+      <h1 className="font-bold hidden md:block lg:block lg:text-2xl">
+        Hero Movies
+      </h1>
 
       <ul className="flex items-center justify-end">
         {session ? (
